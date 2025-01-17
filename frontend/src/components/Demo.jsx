e0a4d75c7f7021c267d9ff5643b4fa0669bc378a
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2025-01-17 07:55:19 -1200

    Worked on the homepage

---
+++ frontend/src/components/Demo.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Demo.css'
+
+const Demo = () => {
+    return (
+        <div className='demo' id='demo'>
+            <div className='container'>
+                <div className='col-1'>
+                    <p>More Than 100 Financial Planners,</p>
+                    <p>One Philosophy</p>
+                    <p>Every single one of our financial advisors receives rigorous training according to John Doe’s philosophy based on academic research (including that of a Nobel laureate in Economics) and Behavioral Finance.</p>
+                    <button className='button'>Get your free financial analysis</button>
+                </div>
+                <div className='col-2'>
+                    <img source src="" type=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Demo
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='content'>
+                <p></p>
+                <p>POSSA</p>
+                <p>Knowledge, Integrity, and <br/> National Development.</p>
+                <p className="context">Join the Political Science Students' Association in shaping tomorrow's leaders <br/> through knowledge, collaboration, and integrity.</p>
+               
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
