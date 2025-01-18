81fce77c02cdeca3867c99ef74d4986b7c9649f3
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2025-01-17 14:39:07 -1200

    Changes

--- frontend/src/components/Demo.jsx
+++ frontend/src/components/Demo.jsx
@@ -7,7 +7,7 @@ const Demo = () => {
             <div className='container'>
                 <div className='col-1'>
                     <p>More Than 100 Financial Planners,</p>
-                    <p>One Philosophy</p>
+                    <p> Philosophy</p>
                     <p>Every single one of our financial advisors receives rigorous training according to John Doe’s philosophy based on academic research (including that of a Nobel laureate in Economics) and Behavioral Finance.</p>
                     <button className='button'>Get your free financial analysis</button>
                 </div>
