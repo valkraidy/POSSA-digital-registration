825e444e3648da196d893e8c8663928059526338
unknown <dorothykotey66@gmail.com>
2025-01-19 14:51:15 -0800

    message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import LoginPage from './components/Login'; 
 import SignUp from './components/SignUp'; 
-<<<<<<< HEAD
-import Registration from './components/Registration'; 
-=======
-import Settings from './components/Settings';
-import Souvenir from './components/Souvenir';
->>>>>>> 9279d6f528b39174ddc2714cbe351e2f7ac7a106
+import Registration from './components/Registration';
 // import Testimonials from './components/Testimonials';
 // import Demo from './components/Demo';
 // import Footer from './components/Footer';
@@ -33,13 +28,10 @@ function App() {
         />
         <Route path="/login" element={<LoginPage />} /> {/* Corrected path */}
         <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
-        <Route path="/Registration" element={<Registration />} />
-=======
         <Route path="/settings" element={<Settings />} />
         <Route path="/souvenir" element={<Souvenir />} />
+        <Route path="/Registration" element={<Registration />} />
 
->>>>>>> 9279d6f528b39174ddc2714cbe351e2f7ac7a106
       </Routes>
     </Router>
   );
