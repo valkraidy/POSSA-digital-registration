54521b077bd69f2db0b94cceaef054bd5e6f6fbb
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2025-01-19 03:46:43 -1200

    Work on about section

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -8,11 +8,18 @@ const About = () => {
             <div className='container'>
                 <img src={about} alt='john' />
                 <div className='col-2'>
-                    <h2>About Possa</h2>
+                    <h2>ABOUT POSSA</h2>
                     <span className='line'></span>
                     <p>We are the Political Science Students Association of the Political Science Department, University of Ghana, Accra City Campus. Guided by our core values of , we welcome you warmly aboard! Together, lets strive for excellence and meaningful impact..</p>
-                    <p>I am John Doe, a senior advisor for an independently owned financial planning company called Intense.</p>
-                     <p>I am John Doe, a senior advisor for an independently owned financial planning company called Intense.</p>
+                    <div className="vision">
+                     <h2>Our Vision</h2>
+                    <p>To investigate and address issues of power, politics, and government, and their bearing on the quality of life and people's hopes and aspirations.</p>
+                    </div>
+
+                    <div className="Mission">
+                     <h2>Our Mission</h2>
+                     <p>To examine the origin, development, and operation of political systems using historical, comparative, interpretative, critical, quantitative and qualitative methods..</p>
+                     </div>
                     <button className='button'>Read More</button>
                 </div>
             </div>
