2e856ecaa2ac4734f6361e7fb00d31903561cf98
Stan1-0 <stankofb@gmail.com>
2025-01-25 13:47:03 +0000

    fixed conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
@@ -8,51 +7,25 @@ import SignUp from './components/SignUp';
 import Registration from './components/Registration';
 import Settings from './components/Settings'
 import Souvenir from './components/Souvenir'
-// import Testimonials from './components/Testimonials';
-// import Demo from './components/Demo';
-// import Footer from './components/Footer';
-//import Welcome from './components/Welcome'; 
+import Homepage from './components/Homepage' 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <About />
-              {/* <Testimonials /> */}
-              {/* <Demo /> */}
-              {/* <Footer /> */}
-            </>
-          }
+          path="/" element={<Homepage/>}
         />
         <Route path="/login" element={<LoginPage />} /> {/* Corrected path */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/souvenir" element={<Souvenir />} />
         <Route path="/Registration" element={<Registration />} />
+        
 
       </Routes>
     </Router>
-=======
-
-
-
-import React from 'react'
-import Homepage from './components/Homepage'
-
-
-function App() {
-  return (
-    <div>
-     
-      <Homepage/>
-    </div>
->>>>>>> 6896a5d17f057c9bd1884e67574eb9317adb201e
-  );
+  )
 }
 
 export default App;
