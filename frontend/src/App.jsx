7a0be6559c935e3d54899c84cb6cc11648404671
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2025-02-03 18:11:23 -1200

    Fix hopepage issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Hero from "./components/Hero";
-// import About from "./components/About";
+
 import Dashboard from "./Pages/Dashboard";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
